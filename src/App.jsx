95d4d9cdb65442af51df9077085614dd4393ad4d
samwissy <samwissyconcept1@gmai.com>
2023-12-09 22:54:26 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world! This is a new beggining for dokidol
+    </h1>
+  );
 }
