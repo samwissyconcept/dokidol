c1ade59453930edf3408a4f7ffa6c6dc4f8b4330
samwissy <samwissyconcept1@gmai.com>
2023-12-09 23:19:09 +0100

    first

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navigation from "./Navigation";
 import Homepage from "./Homepage";
+import Footer from "./Footer";
 
 function Index() {
   return (
     <div>
       <Navigation />
       <Homepage />
+      <Footer />
     </div>
   );
 }
+
+export default Index;
